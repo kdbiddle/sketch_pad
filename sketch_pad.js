491f58e9bb2fb4c4aaa00304427373f7929215db
Kevin Biddle <kdbiddle08@gmail.com>
2015-12-14 22:33:11 -0500

    Adding dynamic creation of multiple HTML elements functionality

--- sketch_pad.js
+++ sketch_pad.js
@@ -1,3 +1,5 @@
+var gridSize;
+
 function newParagraph() {
 	var container = document.getElementById("container");
 	var fadeDiv = document.getElementById("fadein");
@@ -5,8 +7,21 @@ function newParagraph() {
 	container.insertBefore(testParagraph, fadeDiv);
 };
 
+function getGridSize() {
+	gridSize = prompt("Please enter a number between 1 and 10.");
+	parseInt(gridSize);
+	console.log(gridSize);
+};
+
+function createSketchPad() {
+	for (var i=0; i<gridSize; i++) {
+		newParagraph();
+	};
+};
+
 $(document).ready(function() {
 	$(document).on("click", "#newSketchpad", function(){
-		newParagraph();
+		getGridSize();
+		createSketchPad();
 	});
 });
